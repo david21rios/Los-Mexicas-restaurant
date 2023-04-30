f58c96c5c68fa3eefbed7f8ca81a18e1a83263e6
david21rios <121947579+david21rios@users.noreply.github.com>
2023-05-01 00:45:01 +0200

    Unificacion de archivos
    Unificacion de archivos

--- assets/js/main.js
+++ assets/js/main.js
@@ -204,6 +204,37 @@
   });
 
   /**
+   * Menu-drink isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let menuContainer = select('.menu-drink-container');
+    if (menuContainer) {
+      let menuIsotope = new Isotope(menuContainer, {
+        itemSelector: '.menu-drink-item',
+        layoutMode: 'fitRows'
+      });
+
+      let menuFilters = select('#menu-drink-flters li', true);
+
+      on('click', '#menu-drink-flters li', function(e) {
+        e.preventDefault();
+        menuFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        menuIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        menuIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+  /**
    * Initiate glightbox 
    */
   const glightbox = GLightbox({
